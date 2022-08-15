04193ccee882ef7a91be06793e257b768c9f3985
Marcos Henrique <marcosmarcone48@gmail.com>
2022-08-15 15:51:13 -0300

    FEAT: implement home page

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,5 +1,59 @@
-import { Empty } from 'antd';
+import { Layout, Menu } from 'antd';
 
-export default function App() {
-  return <Empty />;
+import Logo from './../../assets/images/badminton-icon.svg';
+import './style.css';
+
+const { Header, Content, Footer } = Layout;
+
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+
+const items = [
+  getItem('Categorias', 'categories', null, [
+    getItem('Cadastrar Categoria', '/register/category'),
+    getItem('Visualizar Categorias', '/categories'),
+  ]),
+  getItem('Atletas', 'athlets', null, [
+    getItem('Cadastrar Atleta ', '/register/athlete'),
+    getItem('Visualizar Atletas', '/athlets'),
+  ]),
+  getItem('Árbitros', 'umpires', null, [
+    getItem('Cadastrar Árbitro', '/register/umpire'),
+    getItem('Visualizar Árbitro', '/umpires'),
+  ]),
+];
+
+export default function HomePage() {
+  function onClick(e) {
+    console.log(e);
+  }
+
+  return (
+    <Layout className='layout'>
+      <Header id='header'>
+        <div className='logo'>
+          <img src={Logo} alt='badminton logo' />
+          <p>Badminton Profile</p>
+        </div>
+      </Header>
+      <Content id='content'>
+        <Menu
+          onClick={onClick}
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['categories', 'athlets', 'umpires']}
+          mode='inline'
+          items={items}
+          id='menu'
+        ></Menu>
+      </Content>
+      <Footer id='footer'>Badminton Profile ©2022 </Footer>
+    </Layout>
+  );
 }
